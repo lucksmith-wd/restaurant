116b018cfb6878081e52c138ad18d6566f56d776
lucksmith-wd <116928478+lucksmith-wd@users.noreply.github.com>
2023-01-22 22:40:06 +0800

    Import all image files in main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,23 @@ import displayHome from './js-modules/home';
 import displayMenuPage from './js-modules/menu';
 import displayContactPage from './js-modules/contact';
 
+import './images/location.png';
+import './images/rest1.jpg';
+import './images/rest2.jpg';
+import './images/rest3.jpg';
+import './images/desserts/brownies.jpg';
+import './images/desserts/cheesecake.jpg';
+import './images/desserts/waffles.jpg';
+import './images/entrees/filet-mignon.jpg';
+import './images/entrees/house-burger.jpg';
+import './images/entrees/ribeye-steak.jpg';
+import './images/starters/caesar-salad.jpg';
+import './images/starters/onion-soup.jpg';
+import './images/starters/shrimp-cocktail.jpg';
+import './images/icons/email-icon.svg';
+import './images/icons/location-icon.svg';
+import './images/icons/phone-icon.svg';
+
 const navItems = [...document.querySelectorAll('.nav__item')];
 const content = document.querySelector('.content');
 
